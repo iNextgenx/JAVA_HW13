63502aef53bcba8608341dd9b52288b66f2187ea
Igor Smirnov <inextgen@mail.ru>
2023-03-25 22:28:18 +0300

    Reformated code @ Epic.java

--- src/main/java/Epic.java
+++ src/main/java/Epic.java
@@ -14,9 +14,10 @@ public class Epic extends Task {
     @Override
     public boolean matches(String query) {
         for (int i = 0; i < subtasks.length; i++) {
-            if (subtasks[i].contains(query)){
+            if (subtasks[i].contains(query)) {
                 return true;
             }
-        }return false;
+        }
+        return false;
     }
 }
